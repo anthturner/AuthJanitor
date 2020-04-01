dd9d886ea36003f3bd1345d0b8a04d6911d9df57
Anthony Turner <225599+anthturner@users.noreply.github.com>
2020-04-01 14:55:34 -0700

    Add Azure Maps RekeyableObjectProvider

---
+++ AuthJanitor.Providers.AzureMaps/AzureMapsConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace AuthJanitor.Providers.AzureMaps
+{
+    public class AzureMapsConfiguration : AuthJanitorProviderConfiguration
+    {
+        public enum AzureMapsKeyType
+        {
+            [Description("Primary")]
+            Primary,
+            [Description("Secondary")]
+            Secondary
+        }
+
+        /// <summary>
+        /// Azure Maps Key Type
+        /// </summary>
+        [DisplayName("Key Type")]
+        public AzureMapsKeyType KeyType { get; set; }
+
+        /// <summary>
+        /// Skip the process of scrambling the other (non-active) key
+        /// </summary>
+        [DisplayName("Skip Scrambling Other Key?")]
+        [Description("If checked, the opposite key (e.g. primary/secondary) will NOT be scrambled at the end of the rekeying")]
+        public bool SkipScramblingOtherKey { get; set; }
+    }
+}
